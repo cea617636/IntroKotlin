a939fb5a2a777545d8c35f47de82f5cc3ce701d8
cristianEstrada <cjestradaandrade@gmail.com>
2024-09-21 08:03:27 -0600

    Se agrega funcionalidad solicitada en la problemática.

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,42 @@
-class Main {
+
+
+fun main(){
+    val alimentos = mapOf(
+        "Pizza" to 800,
+        "Ensalada" to 200,
+        "Hamburguesa" to 700,
+        "Manzana" to 100,
+        "Helado" to 300,
+        "Pasta" to 600,
+        "Yogurt" to 150
+    )
+    var altos = mutableMapOf<String, Int>()
+    var bajos = mutableMapOf<String, Int>()
+    var totalCalorias = 0
+
+    println("--- Lista de alimentos ---")
+    for ((k, v) in alimentos) {
+        totalCalorias += v
+        println("$k: $v")
+        if(v > 500){
+            altos.put(k, v)
+        }else if (v < 500){
+            bajos.put(k, v)
+        }
+    }
+    println("--- -- -- -- -- ---")
+
+    println(" ----- Alimentos altos en calorías ------ ")
+    for((k, v) in altos){
+        println("$k: $v")
+    }
+    println("--- -- -- -- -- ---")
+
+    println(" ----- Alimentos bajos en calorías ------ ")
+    for((k, v) in bajos){
+        println("$k: $v")
+    }
+    println("--- -- -- -- -- ---")
+    println("Total de calorias consumidas: $totalCalorias")
+    println("Total de alimentos altos en calorías: ${altos.size}")
 }
